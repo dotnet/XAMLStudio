92393a40431a2e23875aa5dd86fed276ae97bcc8
Herrick Spencer <herricks@microsoft.com>
2018-07-24 14:34:23 -0700

    PR fixes

--- XamlStudio.Toolkit/Parsers/BindingValue.cs
+++ XamlStudio.Toolkit/Parsers/BindingValue.cs
@@ -1,15 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace XamlStudio.Toolkit.Parsers
 {
-        /// </summary>
     public class BindingValue
     {
-
         /// <summary>
         ///     Gets or sets the path to the binding source property.
         ///
