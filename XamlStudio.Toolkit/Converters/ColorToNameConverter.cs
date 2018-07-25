e2f7ad4552a7fc86f96c9b8e2b40d82e9e962759
Shane Weaver <shweaver@microsoft.com>
2018-07-25 13:42:45 -0700

    Added AlignmentGrid color option

---
+++ XamlStudio.Toolkit/Converters/ColorToNameConverter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Reflection;
+using System.Text;
+using Windows.UI;
+using Windows.UI.Xaml.Data;
+
+namespace XamlStudio.Toolkit.Converters
+{
+    [Bindable]
+    public class ColorToNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null) return null;
+
+            if (value is Color color)
+            {
+                // Look up the name and return it
+                foreach (var colorProps in typeof(Colors).GetRuntimeProperties())
+                {
+                    if (color == (Color)colorProps.GetValue(null))
+                    {
+                        return colorProps.Name;
+                    }
+                }
+
+                // If this color is unknown, output the color hex
+                return new StringBuilder("#")
+                    .Append(color.A.ToString("X2"))
+                    .Append(color.R.ToString("X2"))
+                    .Append(color.G.ToString("X2"))
+                    .Append(color.B.ToString("X2"))
+                    .ToString();
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
