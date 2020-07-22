1086b121c6b15e74ed68599a0e1da6f126b50009
michael-hawker <24302614+michael-hawker@users.noreply.github.com>
2020-07-22 16:39:26 -0700

    Partial-Work on Resource Dictionary

--- XamlStudio.Toolkit/Controls/ResourceViewer/ResourceViewer.cs
+++ XamlStudio.Toolkit/Controls/ResourceViewer/ResourceViewer.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Xml.Linq;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Documents;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using XamlStudio.Toolkit.Extensions;
+using XamlStudio.Toolkit.Services;
 
 namespace XamlStudio.Toolkit.Controls
 {
@@ -29,6 +33,8 @@ namespace XamlStudio.Toolkit.Controls
             get { return (ObservableCollection<ResourceKeyInfo>)GetValue(ResourceKeysProperty); }
             set { SetValue(ResourceKeysProperty, value); }
         }
+
+        public XDocument XmlDocument { get; set; }
 
         // Using a DependencyProperty as the backing store for ResourceKeys.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty ResourceKeysProperty =
@@ -73,12 +79,84 @@ namespace XamlStudio.Toolkit.Controls
 
                         obj.Style = style;
                         keyinfo.DisplayedValueControl = obj;
+
+                        if (style.TargetType.GetProperty("Text") is PropertyInfo text)
+                        {
+                            text.SetValue(obj, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vulputate magna.");
+                        }
+                        else if (style.TargetType.GetProperty("Content") is PropertyInfo content)
+                        {
+                            content.SetValue(obj, "Test Content");
+                        }
                     }
                 }
                 // TODO: Look at xml parsing to grab key/datatype for DataTemplate?
 
                 ResourceKeys.Add(keyinfo);
             }
+
+            if (XmlDocument != null)
+            {
+                // Lookup all x:Key attributes (or Type names if no key)
+                var keys = XmlDocument.Root.Elements().Select(el => GetResourceKey(el)).ToList();
+
+                // Sort ResourceKeys by the XML ordering
+                var rsrc = keys.Join(
+                                    ResourceKeys,
+                                    i => i,
+                                    d => d.Key,
+                                    (i, d) => d).ToArray();
+
+                ResourceKeys.Clear();
+
+                foreach (var res in rsrc)
+                {
+                    ResourceKeys.Add(res);
+                }
+            }
+        }
+
+        private string GetResourceKey(XElement el)
+        {
+            // If it has a key, use that!
+            var key = el.Attributes().GetNamedItem("{http://schemas.microsoft.com/winfx/2006/xaml}Key")?.Value;
+
+            if (key == null)
+            {
+                // Otherwise, most likely we have a target type
+                key = el.Attributes().GetNamedItem("TargetType")?.Value;
+
+                if (key != null)
+                {
+                    // Get the Namespace or use default
+                    var ns = string.Empty;
+                    if (key.IndexOf(':') != -1)
+                    {
+                        var parts = key.Split(':');
+                        ns = parts[0];
+                        key = parts[1];
+                    }
+                    else
+                    {
+                        ns = "Windows.UI.Xaml.Controls";
+                    }
+
+                    // Look up type to get fully-qualified type name
+                    // TODO: We need to use short friendly namespace here...
+                    // Ugh.
+                    if (AppAssemblyInfo.Instance.TypesByNamespace.TryGetValue(ns, out var types))
+                    {
+                        var type = types.FirstOrDefault(t => t.Name == key);
+                        if (type != null)
+                        {
+                            key = type.FullName;
+                        }
+                    }
+
+                }
+            }
+
+            return key;
         }
     }
 }
--- XamlStudio/ViewModels/DocumentViewModel.Methods.cs
+++ XamlStudio/ViewModels/DocumentViewModel.Methods.cs
@@ -145,7 +145,11 @@ namespace XamlStudio.ViewModels
             var element = Result.Element;
             if (Result.IsResourceDictionary)
             {
-                element = new ResourceViewer() { ResourceDictionary = element as ResourceDictionary };
+                element = new ResourceViewer()
+                {
+                    ResourceDictionary = element as ResourceDictionary,
+                    XmlDocument = Result.Document
+                };
             }
 
             // Only Update if we have a new well-parsed element.
