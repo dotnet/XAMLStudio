62c9d45be21b52b38a124d1d9916884d3b9d947c
Michael Hawker <24302614+michael-hawker@users.noreply.github.com>
2025-03-05 16:29:44 -0800

    Setup Initial Layout for a Fiddle/Playground Style Test + Loading Default Template from Files
    Tested with both WASDK and WASM for file loading.

--- XamlStudio3/MainPage.xaml.cs
+++ XamlStudio3/MainPage.xaml.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+
 namespace XamlStudio3;
 
 public sealed partial class MainPage : Page
@@ -5,5 +7,20 @@ public sealed partial class MainPage : Page
     public MainPage()
     {
         this.InitializeComponent();
+
+        Loaded += MainPage_Loaded;
+    }
+
+    private async void MainPage_Loaded(object sender, RoutedEventArgs e)
+    {
+        XamlMarkup.Text = await ReadTemplateTextAsync(@"Templates\Default\BlankPage.txaml");
+        CSharpCode.Text = await ReadTemplateTextAsync(@"Templates\Default\ViewModel.cs");
+        XamlResourceMarkup.Text = await ReadTemplateTextAsync(@"Templates\Default\ResourceDictionary.xaml");
+    }
+
+    private async Task<string> ReadTemplateTextAsync(string relativeFilePath)
+    {
+        var file = await StorageFile.GetFileFromApplicationUriAsync(new Uri(Path.Combine("ms-appx:///", relativeFilePath)));
+        return await FileIO.ReadTextAsync(file);
     }
 }
