3a74b829e578da9b9c103beebbf7db74a1f73e64
Michael Hawker <24302614+michael-hawker@users.noreply.github.com>
2025-03-06 13:28:59 -0800

    [WIP] Almost loads everything correctly in WinUI first time
    Issues:
    - Dynamic ResourceDictionary assignment doesn't work, need to investigate more, setting directly or merging is failing, at least later (element already has parent?), have yet to see it work properly/as expected
    - Uno Code Compilation failing, need to investigate differences with Jerome's code compilation for SkiaSharp in browser with Uno compared to what's working locally here.

---
+++ Readme.md
@@ -0,0 +1,8 @@
+
+# XAML Studio v3
+
+Initial prototype of a playground/fiddle type environment that works with [WinUI and the Windows App SDK](https://aka.ms/winui3), as well as the browser with [Uno Platform](https://platform.uno). Progression from the original ideas around [XAML Studio](https://aka.ms/xamlstudio).
+
+## Inspiration
+
+While XAML Studio's backlog has desired to mix-in C# code for a while now, the practical application of this being possible and demonstrated was inspired by [AndrewKeepCoding's great prototype here](https://github.com/AndrewKeepCoding/DynamicWinUI3SampleApp) as well as [Jerome Laban's Uno SkiaSharp Fiddle here](https://github.com/jeromelaban/uno.skiasharpfiddle).
--- XamlStudio3/MainPage.xaml.cs
+++ XamlStudio3/MainPage.xaml.cs
@@ -1,13 +1,46 @@
+using System.ComponentModel;
+using System.Linq;
 using System.Reflection;
+using System.Runtime.InteropServices;
+using CommunityToolkit.Mvvm.SourceGenerators;
+using CommunityToolkit.WinUI;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Emit;
+using Microsoft.UI.Dispatching;
+using Microsoft.UI.Xaml.Markup;
 
 namespace XamlStudio3;
 
 public sealed partial class MainPage : Page
 {
+    // Cache of all our compilation inputs
+    private static readonly string RuntimePath = RuntimeEnvironment.GetRuntimeDirectory();
+    private static readonly ImmutableArray<MetadataReference> AssemblyReferences = CreateReferences();
+    private static readonly IIncrementalGenerator[] Generators = [
+        new ObservablePropertyGenerator(),
+        new RelayCommandGenerator()
+    ];
+    private static readonly CSharpCompilationOptions CompilationOptions = new(
+                    outputKind: OutputKind.DynamicallyLinkedLibrary,
+                    nullableContextOptions: NullableContextOptions.Enable);
+
+    // Debounce Timers and Interval
+    private readonly DispatcherQueueTimer _debounceTimerXaml = DispatcherQueue.GetForCurrentThread().CreateTimer();
+    private readonly DispatcherQueueTimer _debounceTimerCode = DispatcherQueue.GetForCurrentThread().CreateTimer();
+    private readonly DispatcherQueueTimer _debounceTimerResources = DispatcherQueue.GetForCurrentThread().CreateTimer();
+    private readonly TimeSpan _debounceInterval = TimeSpan.FromMilliseconds(150);
+
+    // Private cached intermediary results
+    private object _viewModel;
+
+    private ResourceDictionary _resources;
+
     public MainPage()
     {
         this.InitializeComponent();
 
+        // TODO: Should spin this off as a task so UI can load and show progress/spinner
         Loaded += MainPage_Loaded;
     }
 
@@ -16,6 +49,17 @@ public sealed partial class MainPage : Page
         XamlMarkup.Text = await ReadTemplateTextAsync(@"Templates\Default\BlankPage.txaml");
         CSharpCode.Text = await ReadTemplateTextAsync(@"Templates\Default\ViewModel.cs");
         XamlResourceMarkup.Text = await ReadTemplateTextAsync(@"Templates\Default\ResourceDictionary.xaml");
+
+        // We want to do initial pass after loading all text in the proper/efficient order.
+        CompileResources();
+        CompileCode();
+        CompileXaml(); // We do this after as it uses the other results
+
+        // Then listen to new/incremental changes.
+        // TODO: Maybe have an IsLoading guard for when we load other samples?
+        XamlMarkup.TextChanged += XamlMarkup_TextChanged;
+        CSharpCode.TextChanged += CSharpCode_TextChanged;
+        XamlResourceMarkup.TextChanged += XamlResourceMarkup_TextChanged;
     }
 
     private async Task<string> ReadTemplateTextAsync(string relativeFilePath)
@@ -23,4 +67,170 @@ public sealed partial class MainPage : Page
         var file = await StorageFile.GetFileFromApplicationUriAsync(new Uri(Path.Combine("ms-appx:///", relativeFilePath)));
         return await FileIO.ReadTextAsync(file);
     }
+
+    private void XamlMarkup_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        _debounceTimerXaml.Debounce(() =>
+        {
+            CompileXaml();
+        }, interval: _debounceInterval);
+    }
+
+    private void CSharpCode_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        _debounceTimerCode.Debounce(() =>
+        {
+            CompileCode();
+        }, interval: _debounceInterval);
+    }
+
+    private void XamlResourceMarkup_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        _debounceTimerResources.Debounce(() =>
+        {
+            CompileResources();
+        }, interval: _debounceInterval);
+    }
+
+    /// <summary>
+    /// Reads the XamlMarkup.Text and modifies the ResultRoot.Child with the result,
+    /// uses the last compiled _resources and _viewModel.
+    /// </summary>
+    private void CompileXaml()
+    {
+        try
+        {
+            // Depends on CompileResources and CompileCode to be most efficient
+            if (XamlReader.Load(XamlMarkup.Text) is FrameworkElement fe)
+            {
+                if (ResultRoot.Child is FrameworkElement oldFe)
+                {
+#if WINDOWS
+                    VisualTreeHelper.DisconnectChildrenRecursive(oldFe);
+#endif
+                    ResultRoot.Child = null;
+                }
+                ResultRoot.Child = fe;
+
+                // Re-hook cached drivers
+                if (_viewModel != null)
+                {
+                    fe.DataContext = _viewModel;
+                }
+
+                if (_resources != null)
+                {
+                    fe.Resources.MergedDictionaries.Clear();
+                    fe.Resources.MergedDictionaries.Add(_resources);
+                }
+            }
+        }
+        catch (Exception exception)
+        {
+            // TODO: Report Errors
+        }
+    }
+
+    /// <summary>
+    /// Compiles the resources and injects the result into the ResultRoot.Child's Resources.
+    /// </summary>
+    private void CompileResources()
+    {
+        try
+        {
+            if (XamlReader.Load(XamlResourceMarkup.Text) is ResourceDictionary rd)
+            {
+                _resources = rd;
+
+                // Update UI
+                if (ResultRoot.Child is FrameworkElement fe)
+                {
+                    fe.Resources.MergedDictionaries.Clear();
+                    fe.Resources.MergedDictionaries.Add(_resources);
+                }
+            }
+        }
+        catch (Exception exception)
+        {
+            // TODO: Report Errors
+        }
+    }
+
+    /// <summary>
+    /// Compiles the code and injects the result into the ResultRoot.Child's DataContext.
+    /// </summary>
+    private void CompileCode()
+    {
+        try
+        {
+            SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(CSharpCode.Text,
+                                                               CSharpParseOptions.Default.WithLanguageVersion(LanguageVersion.Preview));
+
+            CSharpCompilation compilation = CSharpCompilation.Create(
+                assemblyName: "SomeAssembly",
+                syntaxTrees: [ syntaxTree ],
+                references: AssemblyReferences,
+                options: CompilationOptions
+                );
+
+            GeneratorDriver driver = CSharpGeneratorDriver.Create(Generators)
+                .WithUpdatedParseOptions((CSharpParseOptions)syntaxTree.Options);
+
+            // Run Source Generators
+            _ = driver.RunGeneratorsAndUpdateCompilation(compilation, out Compilation outputCompilation, out ImmutableArray<Diagnostic> diagnostics);
+
+            // TODO: Check diagnostic output from source generators
+
+            // Compile to memory
+            using MemoryStream stream = new();
+            EmitResult emitResult = outputCompilation.Emit(stream);
+
+            if (emitResult.Success is false)
+            {
+                // TODO: Errors
+                //// emitResult.Diagnostics;
+                return;
+            }
+
+            _ = stream.Seek(0, SeekOrigin.Begin);
+
+            var assembly = Assembly.Load(stream.ToArray());
+            if (assembly != null && assembly.CreateInstance("ViewModel") is object viewModel)
+            {
+                // TODO: Get/detect class name from input text
+                _viewModel = viewModel;
+            }
+            else
+            {
+                // TODO: Report error
+                return;
+            }
+
+            // Update UI
+            if (ResultRoot.Child is FrameworkElement fe)
+            {
+                fe.DataContext = _viewModel;
+            }
+        }
+        catch (Exception exception)
+        {
+            // TODO: Errors
+        }
+    }
+
+    private static ImmutableArray<MetadataReference> CreateReferences() => new[]
+        {
+            // System References
+            typeof(object).GetTypeInfo().Assembly.Location,
+            Path.Combine(RuntimePath, "System.Collections.dll"),
+            Path.Combine(RuntimePath, "System.Linq.Expressions.dll"),
+            Path.Combine(RuntimePath, "System.Runtime.dll"),
+            typeof(PropertyChangedEventHandler).GetTypeInfo().Assembly.Location,
+
+            // Custom Dependencies
+            typeof(ObservableObject).GetTypeInfo().Assembly.Location,
+            typeof(ObservablePropertyGenerator).GetTypeInfo().Assembly.Location,
+        }.Select(path => MetadataReference.CreateFromFile(path))
+         .Cast<MetadataReference>()
+         .ToImmutableArray();
 }
